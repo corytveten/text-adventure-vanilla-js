be3f3e6c2393f98132a4948108942727499cf278
Cory Tveten <corytveten@gmail.com>
2021-11-18 22:06:14 -0500

    question text and showTextNode

--- game.js
+++ game.js
@@ -11,10 +11,29 @@ function startGame() {
 function showTextNode(textNodeIndex) {
     const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
     textElement.innerText = textNode.text
+    while(optionButtonsElement.firstChild) {
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+    }
+
+    textNode.options.forEach(option => {
+        if (showOption(option)) {
+            const button = document.createElement('button')
+            button.innerText = option.text
+            button.classList.add('btn')
+            button.addEventListener('click', () => selectOption(option))
+            optionButtonsElement.appendChild(button)
+        }
+    })
+}
+
+function showOption(option) {
+    return option.requiredState == null || option.requiredState(state)
 }
 
 function selectOption(option) {
-
+    const nextTextNodeId = option.nextText
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId)
 }
 
 const textNodes = [
@@ -30,6 +49,46 @@ const textNodes = [
             {
                 text: "Leave the goo.",
                 nextText: 2
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: "You venture forth in search of answers to where you are when you come across a merchant.",
+        options: [
+            {
+                text: "Trade the goo for a sword.",
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: { blueGoo: false, sword: true },
+                nextText: 3
+            },
+            {
+                text: "Trade the goo for a shield.",
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: { blueGoo: false, shield: true },
+                nextText: 3
+            },
+            {
+                text: "Ignore the merchant.",
+                nextText: 3
+            },
+        ]
+    },
+    {
+        id: 3,
+        text: 'After leaving the merchant, you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+        options: [
+            {
+                text: 'Explore the castle.',
+                nextText: 4
+            },
+            {
+                text: 'Find a room to sleep at in the town.',
+                textText: 5
+            },
+            {
+                text: 'Find some hay in a stable to sleep in',
+                nextText: 6
             }
         ]
     }
