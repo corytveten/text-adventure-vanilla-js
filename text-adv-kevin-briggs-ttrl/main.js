f3825c3afced1a13de95b7adfed8caca9d1a56ad
Cory Tveten <corytveten@gmail.com>
2021-11-29 22:04:47 -0500

    move data to objects

--- text-adv-kevin-briggs-ttrl/main.js
+++ text-adv-kevin-briggs-ttrl/main.js
@@ -1,3 +1,18 @@
+const story = {
+    attack: {
+        title: "Chapter 1",
+        story: `Once upon a time, the village of Catistan lived in peace. But one day, it was attacked bu Avarice the Angry Aardvark. The Premier of Catistan said, "There's only one person who can save us, !`,
+        choices: [
+            {
+                choice: "Yes, I'm ready to accept!",
+            },
+            {
+                choice: "No, I'd rather play video games."
+            }
+        ]
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("start-button");
     const input = document.getElementById("name-input");
@@ -5,8 +20,23 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener('click', () => {
         content.innerHTML = 
         `
-            <h1>Chapter 2</h1>
-            <p>Once upon a time, the village of Catistan lived in peace. But one day, it was attacked bu Avarice the Angry Aardvark. The Premier of Catistan said, "There's only one person who can save us, ${input.value}!"</p>
+            <h1>${story.attack.title}</h1>
+            <p>${story.attack.story}</p>
+            ${getInputs()}
+            <button>Go Forth!</button>
         `
     })
 })
+
+function getInputs() {
+    let input = ""
+    for(let i=0; i<story.attack.choices.length; i++) {
+        input += `
+            <div>
+                <input id = "radio${i}" type="radio" name="choices"/>
+                <label for="radio${i}">${story.attack.choices[i].choice}</label>
+            </div>
+        `
+    }
+    return input;
+}
